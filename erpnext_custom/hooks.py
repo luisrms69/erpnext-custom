6fc1ab55d78fecf4ed8977d8c11d6d1b08216cef
Luis Montanaro Developer env <admin@appcity.mx>
2022-12-16 17:34:43 -0600

    El rol de presidente corregi el nombre pero veo que se debe quedar grabado el primero como clave, no se actualiza

--- erpnext_custom/hooks.py
+++ erpnext_custom/hooks.py
@@ -193,7 +193,7 @@ fixtures = [
        "Custom Field",
        {"doctype": "Role Profile",
 	"filters": {
-		"name": ["in", "Socio, Presidente Consejo de Administracion, Secretario Consejo de Administracion, Tesorero Consejo de Administracion"]
+		"name": ["in", "Socio, Presidente Consejo Administracion, Secretario Consejo de Administracion, Tesorero Consejo de Administracion"]
               }
        }
        
